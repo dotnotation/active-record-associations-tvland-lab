cec1c64634a7408ef43265cfe52e2f726fc37079
dotnotation <dotinmotionproductions@gmail.com>
2021-07-19 12:25:38 -0400

    commit tree 50ceb7da98f3815695db9e1b4db4e71d2c6ca34e

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+       "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        #list all the characters that belong to the actor and the show name
+        self.characters.map do |character|
+            "#{character.name} - #{character.show.name}"
+        end
+    end
   
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        #returns array of full names of each actor 
+        self.actors.map do |actor|
+            "#{actor.first_name} #{actor.last_name}"
+        end
+    end
 end
